004f45d97c722d6f7cd788dc569d8fd70088c848
sparkeplug <prabhu.rk90@gmail.com>
2015-12-07 17:34:12 +0530

    Adding project files to GIT

---
+++ javascript/routes.js
@@ -0,0 +1,20 @@
+(function(){
+angular.module("NammaThottam")
+    .config(function($routeProvider){
+        $routeProvider.when('/Home',{
+            templateUrl:'../Templates/Home.html'
+        })
+        .when('/Fruits',{
+            templateUrl:'../Templates/Fruits.html'
+        })
+        .when('/Vegetables',{
+            templateUrl:'../Templates/Vegetables.html'
+        })
+        .when('/Contact_Us',{
+            templateUrl:'../Templates/Contact_us.html'
+        })
+        .otherwise({
+            redirectTo:'/Home'
+        });
+    });
+})();
